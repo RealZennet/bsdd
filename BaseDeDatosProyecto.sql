69697050c0b618564e63b652849a1dcea5d3737a
gastonferron <bruceferr@gmail.com>
2023-09-17 22:07:44 -0300

    Final

--- BaseDeDatosProyecto.sql
+++ BaseDeDatosProyecto.sql
@@ -1,6 +1,5 @@
-DROP DATABASE IF EXISTS proyecto_Base_de_Datos;
-CREATE DATABASE proyecto_Base_de_Datos CHARSET utf8mb4;
-USE proyecto_Base_de_Datos;
+CREATE DATABASE quickcarryDB CHARSET utf8mb4;
+USE quickcarryDB;
 
 CREATE TABLE trabajador(
 	id INT UNSIGNED AUTO_INCREMENT,
@@ -8,7 +7,7 @@ CREATE TABLE trabajador(
 	pass VARCHAR(50) NOT NULL,
     nom VARCHAR(30),
 	ape VARCHAR(30),
-	tel TINYINT(11) UNSIGNED,
+	tel VARCHAR(12),
 	bajalogica BOOLEAN DEFAULT 0 NOT NULL,
     CONSTRAINT pk_trabajador PRIMARY KEY (id) 
 );
@@ -22,20 +21,24 @@ CREATE TABLE almacen(
 );
 CREATE TABLE camion(
 	id_camion INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	peso_camion SMALLINT(100),
-    volumen_camion SMALLINT(100),
+	peso_camion SMALLINT,
+    volumen_camion TINYINT,
     bajalogica BOOLEAN DEFAULT 0 NOT NULL,
+    CONSTRAINT check_volCamion CHECK (volumen_camion <= 90),
+    CONSTRAINT check_pesoCamion CHECK (peso_camion <= 26000),
     CONSTRAINT pk_camion PRIMARY KEY (id_camion)
 );
 CREATE TABLE producto(
 	id_prod INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	peso_producto SMALLINT(100) UNSIGNED,
-    volumen_producto SMALLINT(100) UNSIGNED,
+	peso_producto SMALLINT UNSIGNED,
+    volumen_producto TINYINT UNSIGNED,
     calle VARCHAR(30) NOT NULL,
     num VARCHAR(10) NOT NULL,
     esq VARCHAR(30),
     cliente VARCHAR(20) NOT NULL,
     bajalogica BOOLEAN DEFAULT 0 NOT NULL,
+    CONSTRAINT check_volProducto CHECK (volumen_producto <= 90),
+    CONSTRAINT check_pesoProducto CHECK (peso_producto <= 26000),
     CONSTRAINT pk_producto PRIMARY KEY (id_prod)
 );
 CREATE TABLE destino(
@@ -43,6 +46,7 @@ CREATE TABLE destino(
 	calle VARCHAR(30) NOT NULL,
     num VARCHAR(10) NOT NULL,
     esq VARCHAR(30),
+    fech_esti DATE,
     bajalogica BOOLEAN DEFAULT 0 NOT NULL,
     CONSTRAINT pk_destino PRIMARY KEY (id_des)
 );
@@ -125,34 +129,40 @@ CREATE TABLE recorrido(
 	CONSTRAINT pk_contiene PRIMARY KEY(id_des, id_alma)
 );
 
-#CREACION USUARIOS
-DROP USER 'root';
-DROP USER 'admin_bd'; 
-DROP USER 'chofer'; 
-DROP USER 'almacenero';
-  
-SELECT user FROM mysql.user;
-CREATE USER 'root' identified by 'root1234';
-CREATE USER 'admin_bd' identified by 'admin123';
-CREATE USER 'chofer' identified by '1234';
-CREATE USER 'almacenero' identified by '4321';
-
 #PERMISOS_ROOT
 GRANT ALL PRIVILEGES ON proyecto_Base_de_Datos.all TO 'root'@'localhost';
-#PERMISOS_admin_bd
-GRANT SELECT, INSERT, DELETE, UPDATE ON proyecto_Base_de_Datos.add TO 'admin_bd'@'localhost';
-#PERMISOS_chofer
-GRANT SELECT, UPDATE ON proyecto_Base_de_Datos.entregan TO 'chofer'@'localhost';
-GRANT SELECT ON proyecto_Base_de_Datos.destino TO 'chofer'@'localhost';
-GRANT SELECT ON proyecto_Base_de_Datos.conduce TO 'chofer'@'localhost';
-GRANT SELECT ON proyecto_Base_de_Datos.llevan TO 'chofer'@'localhost';
-#PERMISOS_almacenero
-GRANT SELECT, UPDATE ON proyecto_Base_de_Datos.almacena TO 'almacenero'@'localhost';
-GRANT SELECT, UPDATE ON proyecto_Base_de_Datos.pertenece TO 'almacenero'@'localhost';
-GRANT SELECT, UPDATE ON proyecto_Base_de_Datos.llevan TO 'almacenero'@'localhost';
-GRANT SELECT ON proyecto_Base_de_Datos.trabaja TO 'almacenero'@'localhost';
-GRANT SELECT ON proyecto_Base_de_Datos.almacen TO 'almacenero'@'localhost';
-GRANT SELECT ON proyecto_Base_de_Datos.producto TO 'almacenero'@'localhost';
-GRANT SELECT ON proyecto_Base_de_Datos.lote TO 'almacenero'@'localhost';
-GRANT SELECT ON proyecto_Base_de_Datos.camion TO 'almacenero'@'localhost';
-GRANT SELECT ON proyecto_Base_de_Datos.destino TO 'almacenero'@'localhost';
+
+#PERMISOS_ADMIN_BD
+GRANT SELECT, INSERT, DELETE, UPDATE ON quickcarryDB.trabajador TO 'admin_bd'@'localhost';
+GRANT SELECT, INSERT, DELETE, UPDATE ON quickcarryDB.almacen TO 'admin_bd'@'localhost';
+GRANT SELECT, INSERT, DELETE, UPDATE ON quickcarryDB.camion TO 'admin_bd'@'localhost';
+GRANT SELECT, INSERT, DELETE, UPDATE ON quickcarryDB.producto TO 'admin_bd'@'localhost';
+GRANT SELECT, INSERT, DELETE, UPDATE ON quickcarryDB.destino TO 'admin_bd'@'localhost';
+GRANT SELECT, INSERT, DELETE, UPDATE ON quickcarryDB.lote TO 'admin_bd'@'localhost';
+GRANT SELECT, INSERT, DELETE, UPDATE ON quickcarryDB.camionero TO 'admin_bd'@'localhost';
+GRANT SELECT, INSERT, DELETE, UPDATE ON quickcarryDB.operario TO 'admin_bd'@'localhost';
+GRANT SELECT, INSERT, DELETE, UPDATE ON quickcarryDB.conduce TO 'admin_bd'@'localhost';
+GRANT SELECT, INSERT, DELETE, UPDATE ON quickcarryDB.gestiona TO 'admin_bd'@'localhost';
+GRANT SELECT, INSERT, DELETE, UPDATE ON quickcarryDB.almacena TO 'admin_bd'@'localhost';
+GRANT SELECT, INSERT, DELETE, UPDATE ON quickcarryDB.integra TO 'admin_bd'@'localhost';
+GRANT SELECT, INSERT, DELETE, UPDATE ON quickcarryDB.llevan TO 'admin_bd'@'localhost';
+GRANT SELECT, INSERT, DELETE, UPDATE ON quickcarryDB.transporta TO 'admin_bd'@'localhost';
+GRANT SELECT, INSERT, DELETE, UPDATE ON quickcarryDB.recorrido TO 'admin_bd'@'localhost';
+
+#PERMISOS_CAMIONERO
+GRANT SELECT, UPDATE ON quickcarryDB.transporta TO 'camionero'@'localhost';
+GRANT SELECT, UPDATE ON quickcarryDB.recorrido TO 'camionero'@'localhost';
+GRANT SELECT ON quickcarryDB.destino TO 'camionero'@'localhost';
+GRANT SELECT ON quickcarryDB.conduce TO 'camionero'@'localhost';
+GRANT SELECT ON quickcarryDB.llevan TO 'camionero'@'localhost';
+
+#PERMISOS_OPERARIO
+GRANT SELECT, UPDATE ON quickcarryDB.almacena TO 'operario'@'localhost';
+GRANT SELECT, UPDATE ON quickcarryDB.integra TO 'operario'@'localhost';
+GRANT SELECT, UPDATE ON quickcarryDB.llevan TO 'operario'@'localhost';
+GRANT SELECT, UPDATE ON quickcarryDB.lote TO 'operario'@'localhost';
+GRANT SELECT ON quickcarryDB.almacen TO 'operario'@'localhost';
+GRANT SELECT ON quickcarryDB.producto TO 'operario'@'localhost';
+GRANT SELECT ON quickcarryDB.lote TO 'operario'@'localhost';
+GRANT SELECT ON quickcarryDB.camion TO 'operario'@'localhost';
+GRANT SELECT ON quickcarryDB.destino TO 'operario'@'localhost';
